17965a1f335004de0c9cdf51c5b979ea18bfb317
HIMESH-1108 <25110cn486@glbitm.ac.in>
2026-05-30 15:33:00 +0530

    Add basic program to calculate birth year
    In this program , finding the year when a person become 100 year old ,

---
+++ basic program 1.py
@@ -0,0 +1,5 @@
+name=input("enter your name :")
+age=int(input("enter your age :"))
+currentyear=int(input("enter the current year:"))
+birth_year=currentyear - age
+print("yu became 100year old at the year ",birth_year+100)
